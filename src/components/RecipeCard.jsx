364cc80ec1082b2b65c291dc3e5f1a2d46f449a3
SK-0911 <shubham.911k@gmail.com>
2024-10-25 23:36:40 +0530

    Rendering of Card done with API

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,15 +1,21 @@
 import { Heart, HeartPulse, Soup } from 'lucide-react'
 import React from 'react'
 
-const RecipeCard = () => {
+const get2labels = (arr) => {
+    return [arr[0], arr[1]];
+}
+
+const RecipeCard = ({recipe,bg,badge}) => {
+    const healthLabels = get2labels(recipe.healthLabels);
+    
   return (
-    <div className="flex flex-col rounded-md bg-[#ecf7d4] overflow-hidden p-3 relative">
+    <div className={`flex flex-col rounded-md ${bg} overflow-hidden p-3 relative`}>
     <a href="#" className='relative h-32'>
-        <img src="/1.jpg" alt="Recipe Img" 
+        <img src={recipe.image} alt="Recipe Img" 
             className='rounded-md w-full h-full object-cover cursor-pointer'
         />
         <div className="absolute bottom-2 left-2 bg-white rounded-full p-1 cursor-pointer flex items-center gap-1 text-sm text-slate-700">
-            <Soup size={16}/> 4 servings
+            <Soup size={16}/> {recipe.yield} servings
         </div>
 
         <div>
@@ -20,19 +26,19 @@ const RecipeCard = () => {
     </a>
 
     <div className="flex mt-1">
-        <div className="font-bold tracking-wide text-slate-700">Roasted Chicken</div>
+        <div className="font-bold tracking-wide text-slate-700">{recipe.label}</div>
     </div>
-    <p className='my-2 text-slate-700'>Turkish Kitchen</p>
+    <p className='my-2 text-slate-700'>{recipe.cuisineType[0].charAt(0).toUpperCase(0) + recipe.cuisineType[0].slice(1)} Cuisine</p>
 
     <div className="flex gap-2 mt-auto">
-        <div className='flex gap-1 bg-[#d6f497] items-center p-1 rounded-md text-slate-700'>
-            <HeartPulse size={16} />
-            <span className='text-sm tracking-tighter font-bold'>Gluten-free</span>
-        </div>
-        <div className='flex gap-1 bg-[#d6f497] items-center p-1 rounded-md text-slate-700'>
-            <HeartPulse size={16} />
-            <span className='text-sm tracking-tighter font-bold'>Heart-Healthy</span>
-        </div>
+        {
+            healthLabels.map((label, idx) => (
+                <div key={idx} className={`flex gap-1 ${badge}  items-center p-1 rounded-md text-slate-700`}>
+                    <HeartPulse size={16} />
+                    <span className='text-sm tracking-tighter font-bold'>{label}</span>
+                </div>
+            ))
+        }
     </div>
 </div>
   )
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,34 @@
 import { Search } from 'lucide-react'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import RecipeCard from '../components/RecipeCard';
+import { getRandomColor } from "../lib/utils";
 
+const APP_ID = "7b15303f";
+const APP_KEY = "393436a82a9b86438048f0f73b380d6d";
 const HomePage = () => {
+    const [recipes, setRecipes] = useState([]);
+    const [loading, setLoading] = useState([]);
+
+    const fetchRecipes = async (search) => {
+        setLoading(true);
+        setRecipes([]);
+        try{
+            const res = await fetch(
+                `https://api.edamam.com/api/recipes/v2/?app_id=${APP_ID}&app_key=${APP_KEY}&q=${search}&type=public`
+            )
+            const data = await res.json();
+            setRecipes(data.hits);
+
+
+        } catch (error) {
+            console.log(error.message);
+        } finally{
+            setLoading(false);
+        }
+    }
+    useEffect(() => {
+        fetchRecipes("Paneer");
+    }, [])
   return <div className='bg-[#222] p-10 flex-1'>
         <div className="max-w-screen-lg mx-auto">
             <form action="">
@@ -22,13 +48,22 @@ const HomePage = () => {
             </p>
 
             <div className='grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
-                {/* Recipe 1 */}
-                <RecipeCard />
-                <RecipeCard />
-                <RecipeCard />
-                <RecipeCard />
-                <RecipeCard />
-                <RecipeCard />
+                {!loading && 
+                    recipes.map(({recipe}, index) => (
+                        <RecipeCard key={index} recipe={recipe} {...getRandomColor()} />
+                    )
+                )}
+                {loading &&
+                        [...Array(9)].map((_, index) => (
+                            <div key={index} className='flex flex-col gap-4 w-full'>
+                                <div className='skeleton h-32 w-full'></div>
+                                <div className='flex justify-between'>
+                                    <div className='skeleton h-4 w-28'></div>
+                                    <div className='skeleton h-4 w-24'></div>
+                                </div>
+                                <div className='skeleton h-4 w-1/2'></div>
+                            </div>
+                        ))}
             </div>
         </div>
     </div>;
